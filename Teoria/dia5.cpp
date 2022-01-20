7fa12a04059d8d3424772ae064df8066057ae9a4
SagittariusITG <joel.arnaud@itgranada.com>
2022-01-20 17:42:32 +0100

    Dia 5 te cpp

---
+++ Teoria/dia5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string palabra="coche";
+    string palabra2="perro";
+
+    cout << palabra << " " << palabra2 << endl;
+
+    for (int i = 10; i > 0 - 1; i--){
+        cout << i << endl;
+    }
+
+    return 0;
+}
