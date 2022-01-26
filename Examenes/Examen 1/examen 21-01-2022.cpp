98dbf18645be419e9114eb1815f4f25a64a59d52
SagittariusITG <joel.arnaud@itgranada.com>
2022-01-26 13:36:12 +0100

    Correción examen

--- Examenes/Examen 1/examen 21-01-2022.cpp
+++ Examenes/Examen 1/examen 21-01-2022.cpp
@@ -13,33 +13,25 @@ int calcular_dias(int edad_meses){
     return (edad_meses * 30);
 }
 
-int calcular_dientes(int meses){
+int calcular_dientes(int meses, int anio_max){
     int sumar = 0;
+    anio_max = (calcular_meses(anio_max));
     int meses_tiene_top_anios = 96;
-    if(meses <= meses_tiene_top_anios){
-        for (int i = 0; i < meses; i += 5){
-            sumar += i;
-        }
-    }else{ // Aqui he hecho otro bucle para "automatizarlo", si, hay que calcular cuantos meses tiene 8 años, pero no he visto más opciones :(
-        for (int i = 0; i < meses_tiene_top_anios; i += 5){
-            sumar += i;
-        }
+    for (int i = 0; i <= meses && i < anio_max; i += 5){
+        sumar += i;
     }
+
     return sumar;
 }
 
 int main(){
     string miNombre = "Joël Arnaud Carreras";
 
-    int edadTibu;
-    int anio_nacimeinto_tibu;
-    int meses_tiburon;
-    int dias_tiburon;
-
-    int dientes;
-
+    int edadTibu, anio_nacimeinto_tibu, meses_tiburon, dias_tiburon, dientes;
+    
     int anio_actual = 2022;
     int mes_actual = 1;
+    int anio_max = 8;
 
     cout << miNombre << endl << endl;
 
@@ -51,7 +43,7 @@ int main(){
 
     cout << "El tiburón nació en el año " << anio_nacimeinto_tibu << " por lo tanto ha vivido " << meses_tiburon << " meses y " << dias_tiburon << " días." << endl;
 
-    dientes = calcular_dientes(meses_tiburon);
+    dientes = calcular_dientes(meses_tiburon, anio_max);
     cout << "Al tiburón se le han caído " << dientes << " dientes." << endl;
     
     return 0;
