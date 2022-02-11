618b7b2518752dc34421da1d37ae4af0196a1717
SagittariusITG <joel.arnaud@itgranada.com>
2022-02-11 13:10:33 +0100

    Final del dia 12

--- Teoria/Dia12.cpp
+++ Teoria/Dia12.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void cantidad_nums(int &n, string &line){
-    cout << "Num de colroes: "; 
+void long_array(int &n, string &line){
+    cout << "Introduce la longitud del array: "; 
     getline(cin, line);
     n = stoi(line);
 }
 
 void introducir_array(string* colores, int &n){
     for(int i = 0; i < n; i++){
-        cout << "Introduce un color: ";
-        getline(cin, colores[i]);
+        cout << "Introduce un color: "; getline(cin, colores[i]);
     }
 }
 
@@ -20,19 +19,50 @@ void imprimir_array(string* colores, int &n){
     }
 }
 
+double* rellenar_array_notas(int tama){
+    double* array = new double[tama];
+    string line;
+    for(int i = 0; i < tama; i++){
+        cout << "Introduce una nota: ";
+        getline(cin, line);
+        array[i] = stod(line);
+    }
+    return array;
+}
+
+void imprimir_array_double(double* colores, int &n){
+    for(int i = 0; i < n; i ++){
+        cout << colores[i] << " ";
+    }
+}
+
 int main(){
+    // Primera parte
     string* colores = NULL;
     int n;
     string line;
 
-    cantidad_nums(n, line);
+    long_array(n, line);
 
     colores = new string[n];
 
     introducir_array(colores, n);
 
     imprimir_array(colores, n);
+    cout << endl;
+
+    // Segunda parte
+    double* notas;
+    int num;
+    string lline;
+
+    long_array(n, lline);
+    notas = new double[num];
+
+    notas = rellenar_array_notas(n);
+    imprimir_array_double(notas, n);
 
     cout << endl;
+
     return 0;
 }
