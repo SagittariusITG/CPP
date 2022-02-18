68995654c85edcfde883ed2168aafe3f48b88b2f
SagittariusITG <joel.arnaud@itgranada.com>
2022-02-18 11:04:11 +0100

    Examen 18-02-2022

---
+++ Examenes/Examen 3/Examen 18-02-2022.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+void imprimirArray(int array[], int tamanio){
+    for (int i = 0 ; i < tamanio ; i++){
+        cout << array[i] << " ";
+    }
+}
+
+void delete_array(int array[], int tamanio){
+    for(int i = 1; i < 5; ++i){
+        array[i] = array[i + 1];
+    }
+}
+
+void lista_al_reves(int array[], int tamanio){
+    for(int i = tamanio - 1; i > 0 - 1; i--){
+        cout << array[i] << " ";
+    }
+}
+
+void copy_array(int array[], int arr[], int size){
+    for(int i = 0; i < size; i++){
+        arr[i] = array[i];
+    }
+}
+
+void order_array(int array[], int ordered[], int size){
+    int cont, greater;
+    cont = 0;
+    greater = 0;
+    for(int i = 0; i < size; i++){
+        if(array[i] > greater){
+            greater = array[i];
+            cont++;
+        }
+    }
+}
+
+// void non_rep_nums(int array[], int size){
+//     for(int i = 0; i < size; i++){
+//         if(array[i] == array[i]){
+//             delete_array(array[i], 4);
+//         }
+//     }
+// }
+
+int main(){
+    string nombre;
+    int rands[5]= {7,3,6,8,2}, copia_array[4], ordered[4];
+    int cantidad;
+
+    // Ejercicio 1
+    nombre = "Joël Arnaud Carreras";
+    cout << nombre;
+    cout << endl << endl;
+
+    // Ejercicio 2
+    cout << "Los números random son: ";
+    imprimirArray(rands, 5);
+    cout << endl << endl;
+
+    // Ejercicio 3
+    rands[2] = rands[2]*2;
+    cout << "El doble del tercer número es: ";
+    imprimirArray(rands, 5);
+    cout << endl << endl;
+
+    // Ejercicio 4
+    cantidad = 4;
+    delete_array(rands, 5);
+    cout << "Eliminando el segundo número: ";
+    imprimirArray(rands, cantidad);
+    cout << endl << endl;
+
+    // Ejercicio 5
+    cout << "El array al reves es: ";
+    lista_al_reves(rands, cantidad);
+    cout << endl << endl;
+
+    // Ejercicio 6
+    copy_array(rands, copia_array, cantidad);
+    cout << "La copia de los numeros random es: ";
+    imprimirArray(copia_array, cantidad);
+    cout << endl << endl;
+
+    // Ejercicio 7
+    order_array(rands, ordered, cantidad);
+    cout << "Los números ordenados son: ";
+    imprimirArray(ordered, cantidad);
+
+    cout << endl;
+    return 0;
+}
---
+++ Examenes/Examen 3/README.md
@@ -0,0 +1,76 @@
+# Examen Array
+
+Este es el examen de c++ sobre arrays sin clase vector.
+
+## Preguntas
+
+1. Imprime tu nombre por pantalla.
+2. Crea un array inicializado por defecto con 5 números aleatorios entre el 0 y el 10.
+3. Cambiar el tercer número por su doble.
+4. Elimina el segundo número. (Ej: [1,2,3,4,5] -> [1,3,4,5])
+5. Imprime por pantalla directamente la lista al revés.
+6. Guarda en una lista los números al revés de como se encuentran en la lista original. (Ej: [1,2,3,4,5] -> [5,4,3,2,1]
+7. Guarda una lista con los números ordenados.
+8. Guarda una lista sin los números repetidos.
+9. Crea una segunda lista solicitando los números por pantalla. Únela a la primera y elimina los números repetidos.
+10. De nuevo con esa segunda lista y la primera, imprime solo los números que aparezcan en las dos listas.
+
+## Funciones
+
+> Funcion para imprimir array
+
+```cpp
+void imprimirArray(int array[], int tamanio){
+    for (int i = 0 ; i < tamanio ; i++){
+        cout << array[i] << " ";
+    }
+}
+```
+
+> Función para eliminar una posición de un array
+
+```cpp
+void delete_array(int array[], int tamanio){
+    for(int i = 1; i < 5; ++i){
+        array[i] = array[i + 1];
+    }
+}
+```
+
+> Imprime un array al revés
+
+```cpp
+void lista_al_reves(int array[], int tamanio){
+    for(int i = tamanio - 1; i > 0 - 1; i--){
+        cout << array[i] << " ";
+    }
+}
+```
+
+> Copia el contenido de un array en otro array
+
+```cpp
+void copy_array(int array[], int arr[], int size){
+    for(int i = 0; i < size; i++){
+        arr[i] = array[i];
+    }
+}
+```
+
+> Ordenar array de mayor a menor
+
+```cpp
+void order_array(int array[], int ordered[], int size){
+    int cont, greater;
+    cont = 0;
+    greater = 0;
+    for(int i = 0; i < size; i++){
+        if(array[i] > greater){
+            greater = array[i];
+            cont++;
+        }
+    }
+}
+```
+
+No funciona
