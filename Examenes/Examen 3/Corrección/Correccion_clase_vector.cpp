98663b833d126781613217646a9b67a7e54495d4
SagittariusITG <joel.arnaud@itgranada.com>
2022-03-04 19:15:07 +0100

    Corrección clase vector

---
+++ Examenes/Examen 3/Corrección/Correccion_clase_vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void n(){
+    vector<string> nombre;
+    nombre = {"casa"};
+    cout << nombre[0];
+}
+
+int main(){
+    // Ejercicio 1
+    n();
+    cout << endl << endl;
+
+    
+
+    cout << endl;
+    return 0;
+}
