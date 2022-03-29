e32f3ad3dcb57531d7c7e3d82327476a277a1cec
SagittariusITG <joel.arnaud@itgranada.com>
2022-03-29 10:42:08 +0200

    Deberes de la clase tablero

---
+++ Deberes/Deberes 17-03-2022/Tablero/README.md
@@ -0,0 +1,23 @@
+# Ejercicios
+
+Estos son los ejercicios con fecha de entrega: 28 lun, 23:59.
+
+## Enunciados 📄
+
+En mi GitHub podéis encontrar el tablero base, partiendo de él podeis completar las tareas que se pedían en la entrega anterior, pero antes de hacerlas os recomiendo jugar un poco con el manero de las posiciones:
+
+* Rellenar el tablero con todo 0s.
+* Rellenar el tablero con un valor dado por pantalla.
+* Rellenar el tablero de forma que cada posición guarde el número de la fila en la que se encuentra.
+* Rellenar el tablero de forma que cada posición guarde el número de la columna en la que se encuentra.
+* Rellenar el tablero de forma que cada posición guarde el número resultado de sumar su fila y su columna.
+
+* Rellenar el tablero de forma que cada posición guarde el número resultado de multiplicar su fila y su columna.
+
+* Rellenar el tablero de forma que cada posición un valor alterno, es decir :
+
+Ej.     BNBNBN
+        NBNBNB
+        BNBNBN
+
+## Funciones 🛠
--- Deberes/Deberes 17-03-2022/Tablero/tablero.cpp
+++ Deberes/Deberes 17-03-2022/Tablero/tablero.cpp
@@ -2,47 +2,57 @@
 using namespace std;
 
 class Tablero{
-private:
+    private:
     int filas;
     int columnas;
     string **tablero;
 
     void imprimirLinea(){
-      for (int j = 0 ; j < this->columnas ; j++){
-        cout << "--" ;
-      }
-      cout << "-" << endl;
+        for (int j = 0 ; j < this->columnas ; j++){
+            cout << "--" ;
+        }
+        cout << "-" << endl;
     }
 
-  public:
+    public:
     Tablero(){
-      this->filas = 8;
-      this->columnas = 8;
-      tablero = new string*[filas];
+        this->filas = 8;
+        this->columnas = 8;
+        tablero = new string*[filas];
 
-      for (int i = 0 ; i < filas ; i++){
-        tablero[i] = new string[columnas];
-      }
+        for (int i = 0 ; i < filas ; i++){
+            tablero[i] = new string[columnas];
+        }
     }
 
     void rellenarTablero(string relleno){
-      for (int i = 0 ; i < this->filas ; i++){
-        for (int j = 0 ; j < this->columnas ; j++){
-          tablero[i][j] = relleno;
+        for (int i = 0 ; i < this->filas ; i++){
+            for (int j = 0 ; j < this->columnas ; j++){
+                tablero[i][j] = relleno;
+            }
         }
-      }
+    }
+
+    void rellenarTableroValorCol(){
+        int contadorF, contadorC = 0;
+        for (int i = 0 ; i < this->filas ; i++){
+            for (int j = 0 ; j < this->columnas ; j++){
+                tablero[i][j] = contador;
+            }
+            contador += 1;
+        }
     }
 
     void imprimir(){
-      this->imprimirLinea();
-      for (int i = 0 ; i < this->filas ; i++){
-        cout << "|";
-        for (int j = 0 ; j < this->columnas ; j++){
-          cout << tablero[i][j] << "|";
-        }
-        cout << endl;
         this->imprimirLinea();
-      }
+        for (int i = 0 ; i < this->filas ; i++){
+            cout << "|";
+            for (int j = 0 ; j < this->columnas ; j++){
+                cout << tablero[i][j] << "|";
+            }
+            cout << endl;
+            this->imprimirLinea();
+        }
     }
 
 };
@@ -61,5 +71,10 @@ int main(){
     tablero.rellenarTablero(valor);
     tablero.imprimir();
 
-  return 0;
+    cout << endl << "Tablero posición fila" << endl << endl;
+
+    tablero.rellenarTableroValorCol();
+    tablero.imprimir();
+
+    return 0;
 }
